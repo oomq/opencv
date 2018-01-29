b336fe85cb06d20ca3f7c0dd62bf202581434db5
oomq_win10 <1543485515@qq.com>
2018-01-29 19:23:11 +0800

    高级保存设置

---
+++ learngit/learngit/learngit2.cpp
@@ -0,0 +1,15 @@
+﻿// learngit.cpp: 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+int main()
+{
+	int a;
+	std::cout << "这是中文吗" << std::endl;
+
+	std::cin >> a ;
+    return 0;
+}
+
